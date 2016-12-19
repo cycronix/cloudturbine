65c960eb178263e5715cc5d0bdd6311a100a8e2d
jpw-erigo <john.wilson@erigo.com>
2016-12-19 17:02:16 -0500

    Add JPEG image compression

--- JavaCode/CTscreencap/src/main/java/erigo/ctscreencap/ScreencapTask.java
+++ JavaCode/CTscreencap/src/main/java/erigo/ctscreencap/ScreencapTask.java
@@ -26,22 +26,41 @@ import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
+import java.util.Iterator;
 import java.util.TimerTask;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
+import javax.imageio.stream.ImageOutputStream;
 
 import cycronix.ctlib.CTwriter;
+
+//
+// This class handles the periodic screen capture.
+//
+// Initial code based on the example found at:
+// http://www.codejava.net/java-se/graphics/how-to-capture-screenshot-programmatically-in-java
+//
 
 public class ScreencapTask extends TimerTask {
 	
 	public CTscreencap cts = null;
 	public CTwriter ctw = null;
 	public int numScreenCaps = 0;   // number of screen captures that have been performed
+	public JPEGImageWriteParam jpegParams = null;
 	
 	// Constructor
 	public ScreencapTask(CTscreencap ctsI) {
 		cts = ctsI;
 		ctw = cts.ctw;
+		
+		// Setup compression
+		jpegParams = new JPEGImageWriteParam(null);
+		jpegParams.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+		jpegParams.setCompressionQuality(cts.imageCompression);
 	}
 	
 	// Perform the work of the screen capture
@@ -63,8 +82,8 @@ public class ScreencapTask extends TimerTask {
 		try {
 			// Get screen image
 			Robot robot = new Robot();
-			Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-			BufferedImage screenCap = robot.createScreenCapture(screenRect);
+			Rectangle captureRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+			BufferedImage screenCap = robot.createScreenCapture(captureRect);
 			// Add mouse cursor to the image
 			int mouse_x = MouseInfo.getPointerInfo().getLocation().x;
 			int mouse_y = MouseInfo.getPointerInfo().getLocation().y;
@@ -72,19 +91,36 @@ public class ScreencapTask extends TimerTask {
 			graphics2D.drawImage(cts.cursor_img, mouse_x, mouse_y, null);
 			// Write the image to CloudTurbine file
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			ImageIO.write(screenCap, "jpg", baos);
-			byte[] jpeg = baos.toByteArray();
+			//
+			// Method 1: To write out an image using the default compression
+			//
+			// ImageIO.write(screenCap, "jpg", baos);
+			//
+			// Method 2: To change the default image compression
+			//   See the following websites for where I got this code:
+			//   http://stackoverflow.com/questions/13204432/java-how-to-set-jpg-quality
+			//   http://stackoverflow.com/questions/17108234/setting-jpg-compression-level-with-imageio-in-java
+			//
+			ImageOutputStream  ios =  ImageIO.createImageOutputStream(baos);
+		    Iterator<ImageWriter> iter = ImageIO.getImageWritersByFormatName("jpg");
+		    ImageWriter writer = iter.next();
+		    writer.setOutput(ios);
+		    writer.write(null, new IIOImage(screenCap,null,null),jpegParams);
+		    writer.dispose();
+			baos.flush();
+			byte[] jpegByteArray = baos.toByteArray();
 			baos.close();
 			System.out.print(".");
 			if ((numScreenCaps%20)==0) {
 				System.err.print("\n");
 			}
-			if ( !cts.bShutdown && (ctw != null) ) {
-				ctw.putData(cts.channelName,jpeg);
-			}
+			ctw.putData(cts.channelName,jpegByteArray);
 		} catch (Exception e) {
-			System.err.println("\nError processing screen capture:\n" + e);
-			e.printStackTrace();
+			if ( !cts.bShutdown && (ctw != null) ) {
+				// Only print out error messages if we know we aren't in shutdown mode
+			    System.err.println("\nError processing screen capture:\n" + e);
+			    e.printStackTrace();
+			}
 			return;
 		}
 		
