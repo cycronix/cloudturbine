7543dcb840646e6291df99672d414446ecd73f82
jpw-erigo <john.wilson@erigo.com>
2017-02-28 11:28:32 -0500

    CTsink: (a) update Apache header; (b) bug fix

--- JavaCode/CTexample/src/main/java/CTsink.java
+++ JavaCode/CTexample/src/main/java/CTsink.java
@@ -1,4 +1,18 @@
+/*
+Copyright 2014-2017 Cycronix
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 import cycronix.ctlib.*;
 
@@ -9,25 +23,6 @@ import cycronix.ctlib.*;
  * @version 10/19/2015
  * 
 */
-
-/*******************************************************************************
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *******************************************************************************/
 
 public class CTsink {
 
@@ -49,7 +44,7 @@ public class CTsink {
 						float[] dd = data.getDataAsFloat32();
 						for(int i=0; i<dd.length; i++) System.err.println("time: "+dt[i]+", data: "+dd[i]);	
 					}
-					if(chan.endsWith(".num")) {
+					else if(chan.endsWith(".num")) {
 						float[] dd = data.getDataAsNumericF32();
 						for(int i=0; i<dd.length; i++) System.err.println("time: "+dt[i]+", data: "+dd[i]);	
 					}
