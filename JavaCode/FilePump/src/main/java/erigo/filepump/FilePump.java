0168a689f8705e949e98f842c01eef2c97b7da05
jpw-erigo <john.wilson@erigo.com>
2017-03-30 10:27:27 -0400

    FilePump: got rid of some imports that were not needed

--- JavaCode/FilePump/src/main/java/erigo/filepump/FilePump.java
+++ JavaCode/FilePump/src/main/java/erigo/filepump/FilePump.java
@@ -35,7 +35,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.Random;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -60,8 +59,6 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.vfs2.impl.StandardFileSystemManager;
 
 public class FilePump implements ActionListener {
 	
