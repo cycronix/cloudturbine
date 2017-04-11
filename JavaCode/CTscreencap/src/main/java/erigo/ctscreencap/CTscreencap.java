2aeca63817bf63e0579e1e3ed03327a49e605e10
jpw-erigo <john.wilson@erigo.com>
2017-04-11 09:08:05 -0400

    CTscreencap changes: (a) default flush interval = 1sec; (b) AudiocapTask tweak to avoid audio buffer overflow

--- JavaCode/CTscreencap/src/main/java/erigo/ctscreencap/CTscreencap.java
+++ JavaCode/CTscreencap/src/main/java/erigo/ctscreencap/CTscreencap.java
@@ -183,7 +183,7 @@ public class CTscreencap implements ActionListener,ChangeListener,MouseMotionLis
 	
 	public final static double DEFAULT_FPS = 5.0;         // default frames/sec
 	public final static Double[] FPS_VALUES = {0.1,0.2,0.5,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0};
-	public final static double AUTO_FLUSH_DEFAULT = 2.0;  // default auto-flush in seconds
+	public final static double AUTO_FLUSH_DEFAULT = 1.0;  // default auto-flush in seconds
 	
 	//
 	// Settings
