4066da636ae37c7c37529920c9b150091235f8e9
jpw-erigo <john.wilson@erigo.com>
2017-05-08 17:16:13 -0400

    CTstream: add the new AudioStream class

--- JavaCode/CTstream/src/main/java/erigo/ctstream/AudioStream.java
+++ JavaCode/CTstream/src/main/java/erigo/ctstream/AudioStream.java
@@ -19,122 +19,170 @@ package erigo.ctstream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
-
-import cycronix.ctlib.CTwriter;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
- * 
- * Capture audio stream, write to CT.
+ * A DataStream for capturing audio data.
+ *
  * @author Matt Miller, Cycronix
+ * @version 2017-05-08
  *
  */
-public class AudiocapTask {
+public class AudioStream extends DataStream implements Runnable {
 
-	protected boolean running;
-	int frequency = 8000; 				//8000, 22050, 44100
-	public Thread captureThread = null; // JPW, so we know when the run() method is done
-	
+	private int frequency = 8000; 		// audio sample frequency (8000, 22050, 44100)
+	private AudioFormat format = null;	// audio format
+	private TargetDataLine line = null;	// audio line
+	private Thread audioThread = null;	// audio capture is performed in this thread, which executes in the run() method
+
+	/**
+	 * AudioStream constructor
+	 *
+	 * @param ctsI   CTstream object
+	 * @param nameI  Channel name
+	 */
 	// constructor
-	public AudiocapTask(CTstream cts, CTwriter ctw, long flushMillis) {
+	public AudioStream(CTstream ctsI, String nameI) {
+		name = nameI;
+		cts = ctsI;
+		bCanPreview = false;
+		bManualFlush = true;
+	}
 
-		try {		
-			final AudioFormat format = getFormat();
-
+	/**
+	 * Implementation of the abstract start() method from DataStream
+	 */
+	public void start() throws IllegalStateException {
+		if (queue != null) { throw new IllegalStateException("ERROR in AudioStream.start(): LinkedBlockingQueue object is not null"); }
+		// Make sure we can open the audio line
+		try {
+			format = getFormat();
 			DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
-			final TargetDataLine line = (TargetDataLine)AudioSystem.getLine(info);
-
+			line = (TargetDataLine) AudioSystem.getLine(info);
 			line.open(format);
 			line.start();
-			Runnable runner = new Runnable() {
-				double flushInterval = flushMillis / 1000.;
-				long oldTime = 0;
-				int bufferSize = (int)Math.round(format.getSampleRate() * format.getFrameSize() * flushInterval);	// bytes per flushMillis
-				int bufferAlloc = bufferSize * (int)(Math.max(1., flushInterval) / flushInterval);					// allocate 1sec min
-				byte buffer[] = new byte[4*bufferAlloc];															// generous buffer to avoid overflow?
-				
-				public void run() {
-					int numFlushes = 0;
-					running = true;
+		} catch (LineUnavailableException e) {
+			throw new IllegalStateException("Audio stream error, could not open the audio line:\n" + e.getMessage());
+		}
+		bIsRunning = true;
+		// Make sure there is no other audio capture running
+		stopAudio();
+		queue = new LinkedBlockingQueue<TimeValue>();
+		// start the audio capture
+		audioThread = new Thread(this);
+		audioThread.start();
+	}
+
+	/**
+	 * Implementation of the abstract stop() method from DataStream
+	 */
+	public void stop() {
+		super.stop();
+		stopAudio();
+	}
+
+	/**
+	 * Implementation of the abstract update() method from DataStream
+	 *
+	 * This method is called when there has been some real-time change to the UI settings that may affect this class
+	 */
+	public void update() {
+		super.update();
+		if (!bIsRunning) {
+			// Not currently running; just return
+			return;
+		}
+		// For AudioStream, there are no other real-time settings we need to adjust for; just return
+		return;
+	}
+
+	/**
+	 * Acquire audio data and put it on the data queue.
+	 */
+	public void run() {
+		double flushInterval = cts.flushMillis / 1000.;
+		int bufferSize = (int) Math.round(format.getSampleRate() * format.getFrameSize() * flushInterval);    // bytes per flushMillis
+		int bufferAlloc = bufferSize * (int) (Math.max(1., flushInterval) / flushInterval);                    // allocate 1sec min
+		byte buffer[] = new byte[4 * bufferAlloc];                                                            // generous buffer to avoid overflow?
+		int numFlushes = 0;
+		try {
+			long nextTime = cts.getNextTime();
+			System.err.println("\n\nWARNING\nWARNING AudioStream: SHOULD WE CALL ctw.setTime() TO ESTABLISH START OF FIRST BLOCK?\nWARNING\n");
+			// ctw.setTime(nextTime);        // establish start time of first audio block
+			while (bIsRunning) {
+				int count = line.read(buffer, 0, bufferSize); // blocking call to read a buffer's worth of audio samples
+				// Slurp up any extra; limit the number of bytes to slurp up so buffer doesn't overflow
+				int numExtraBytesToSlurpUp = line.available();
+				// System.err.println("\ngot " + count + " audio bytes; there are still " + numExtraBytesToSlurpUp + " bytes available; buffer size = " + buffer.length);
+				if (numExtraBytesToSlurpUp > (buffer.length - count)) {
+					// To not overflow the buffer: limit number of extra bytes to slurp up
+					numExtraBytesToSlurpUp = buffer.length - count;
+				}
+				if (numExtraBytesToSlurpUp > 0) {
+					// slurp up extra bytes that are available
+					count += line.read(buffer, count, numExtraBytesToSlurpUp);
+				}
+				// drop whole buffer if below threshold?
+				// if(!audioThreshold(buffer, 100)) continue;
+				// webscan not up to task of handling empty data in RT
+
+				// Save the audio data
+				if (count > 0) {
 					try {
-						// long nextTime = System.currentTimeMillis();
-						long nextTime = cts.getNextTime();
-						ctw.setTime(nextTime);		// establish start time of first audio block
-						oldTime = nextTime;
-						
-						while (running) {
-							int count = line.read(buffer, 0, bufferSize); // blocking call to read a buffer's worth of audio samples
-							// Slurp up any extra; limit the number of bytes to slurp up so buffer doesn't overflow
-							int numExtraBytesToSlurpUp = line.available();
-							// System.err.println("\ngot " + count + " audio bytes; there are still " + numExtraBytesToSlurpUp + " bytes available; buffer size = " + buffer.length);
-							if (numExtraBytesToSlurpUp > (buffer.length - count)) {
-								numExtraBytesToSlurpUp = buffer.length - count;
-								// System.err.println("\tlimiting extra bytes to slurp up to " + numExtraBytesToSlurpUp);
-							}
-							if (numExtraBytesToSlurpUp > 0) {
-								count += line.read(buffer, count, numExtraBytesToSlurpUp);
-							}
-							// if(!audioThreshold(buffer, 100)) continue;		// drop whole buffer if below threshold?
-							// webscan not up to task of handling empty data in RT
-							// long time = System.currentTimeMillis();
-							long time = cts.getNextTime();
-
-							if (count > 0) {
-								// CTput images in timerange of current audio buffer
-								TimeValue tv;
-								while ((tv = cts.screencapStream.queue.poll()) != null) {
-									long imageTime = tv.time;
-									if (imageTime < oldTime) continue;        // discard too-old
-									if (imageTime > time) break;                // out of range (save one?)
-									ctw.setTime(imageTime);
-									ctw.putData(cts.channelName, tv.value);
-									System.out.print("Ax");
-									numFlushes += 1;
-									if ((numFlushes % 40) == 0) {
-										System.err.print("\n");
-									}
-								}
-
-								// CTput audio buffer
-								ctw.setTime(time);
-								ctw.putData(cts.audioChannelName, addWaveHeader(buffer, count));
-								ctw.flush(true);        // gapless
-							}
-
-							oldTime = time;
-						}
-						System.err.println("Closing AudiocapTask");
-						line.close();
+						queue.put(new TimeValue(cts.getNextTime(), addWaveHeader(buffer, count)));
+						System.out.print("a");
 					} catch (Exception e) {
-						System.err.println("\nAudiocapTask I/O problem:");
-						e.printStackTrace();
-						System.exit(-1);
+						if (bIsRunning) {
+							System.err.println("\nAudioStream: exception thrown adding data to queue:\n" + e);
+							e.printStackTrace();
+						}
+						break;
 					}
 				}
-			};
-
-			captureThread = new Thread(runner);
-			captureThread.start();
-		} 
-		catch (LineUnavailableException e) {
-			System.err.println("Line unavailable: " + e);
-			System.exit(-2);
-		}
-		catch(Exception e) {
-			System.err.println("CT error: "+e);
-			System.exit(-1);
+			}
+			// System.err.println("Stopping audio capture");
+		} catch (Exception e) {
+			System.err.println("\nAudioStream I/O problem:");
+			e.printStackTrace();
+			System.err.println("Exiting from audio capture");
 		}
 	}
 
-	public void shutDown() {
-		running = false;
+	/**
+	 * Stop a running audio capture
+	 */
+	private void stopAudio() {
+		if (audioThread == null) {
+			return;
+		}
+		try {
+			// Wait for the thread to finish
+			audioThread.join(4* cts.flushMillis); // audio capture isn't necessarily precise, so wait up to 4*flushMillis
+			if (audioThread.isAlive()) {
+				// the thread must be held up; interrupt it
+				audioThread.interrupt();
+				audioThread.join(500);
+			}
+			if (!audioThread.isAlive()) {
+				// System.err.println("AudioStream has stopped");
+			}
+		} catch (InterruptedException ie) {
+			System.err.println("Caught exception trying to stop AudioStream:\n" + ie);
+		}
+		audioThread = null;
+		// close the audio line
+		line.close();
 	}
-	
+
+	/**
+	 * Specify the audio format: sample rate, sample size, number of channels, byte format
+	 * @return the AudioFormat object
+	 */
 	private AudioFormat getFormat() {
 		float sampleRate = frequency;
 		int sampleSizeInBits = 16;
@@ -144,9 +192,17 @@ public class AudiocapTask {
 		return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
 	}
 
+	/**
+	 * Add a ".wav" header to the audio data.
+	 *
+	 * @param dataBuffer    the raw audio data
+	 * @param totalAudioLen length of the audio data
+	 * @return the updated audio data including ".wav" header
+	 * @throws IOException
+	 */
 	private byte[] addWaveHeader(byte[] dataBuffer, int totalAudioLen) throws IOException {
 		byte RECORDER_BPP = 16;
-//		long totalAudioLen = dataBuffer.length;
+		// long totalAudioLen = dataBuffer.length;
 		long totalDataLen = totalAudioLen + 36;
 		long longSampleRate = frequency;
 		int channels = 1;
@@ -206,9 +262,15 @@ public class AudiocapTask {
 
 		return waveBuffer;
 	}
-	
-	// check if audio buffer has sound volume above threshold
-	boolean audioThreshold(byte[] bytes, int threshold) {
+
+	/**
+	 * Check if audio buffer has sound volume above threshold
+	 *
+	 * @param bytes		the audio data
+	 * @param threshold	desired threshhold
+	 * @return false if all audio data is below the given threshhold
+	 */
+	private boolean audioThreshold(byte[] bytes, int threshold) {
 		// to turn bytes to shorts as either big endian or little endian. 
 		short[] shorts = new short[bytes.length/2];
 		ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(shorts);
