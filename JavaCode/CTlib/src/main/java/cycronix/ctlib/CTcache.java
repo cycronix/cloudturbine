482498f9412bdbd29386ae8839f0c21c9758cf49
Matt Miller <matt@cycronix.com>
2017-07-06 12:08:42 -0400

    CTcache: more robust removeEldest logic
    too many open files could happen with default logic.  now explicitly
    loop through close and remove old cache entries

--- JavaCode/CTlib/src/main/java/cycronix/ctlib/CTcache.java
+++ JavaCode/CTlib/src/main/java/cycronix/ctlib/CTcache.java
@@ -34,7 +34,6 @@ import java.util.zip.ZipFile;
 
 public class CTcache {
 
-	
 	//---------------------------------------------------------------
 	// cache functions
 	
@@ -45,44 +44,56 @@ public class CTcache {
 	private static final int MAX_FILESIZE = 20000000;		// 20MB.  max size any individual entry
 //	private static final int MAX_JVMSIZE = 2000000000;		// 200MB. max overall JVM memory use at which to dump old entries  (2GB?)
 	private static final double MAX_MEMUSE = 0.9;			// fraction available JVM memory to use before limiting cache
-	private static final int MAX_FILES = 100;				// max number open zip files
+	private static final int MAX_ZIPFILES = 1000;			// max number open zip files
 	
 	private static boolean cacheProfile = false;
 
 	// ZipMapCache has small effect versus rebuilding map every time (versus caching zipmap file object itself)
 	static Map<String, Map<String, String[]>>ZipMapCache = new LinkedHashMap<String, Map<String, String[]>>() {
-		 protected boolean removeEldestEntry(Map.Entry  eldest) {
-			 	CTinfo.debugPrint(cacheProfile,"ZipCache stats, size: "+size());
-	            return size() >  MAX_ENTRIES;
+		 protected synchronized boolean removeEldestEntry(Map.Entry  eldest) {
+			 	CTinfo.debugPrint(cacheProfile, "ZipMapCache size: "+size());
+	            return size() >  MAX_ZIPFILES;
 	         }
 	};		
 	
 	// DataCache supplants OS/disk caching for full-file reads
 	static LinkedHashMap<String, byte[]> DataCache = new LinkedHashMap<String, byte[]>() {
-		 protected boolean removeEldestEntry(Map.Entry  eldest) {
+		 protected synchronized boolean removeEldestEntry(Map.Entry  eldest) {
 			 	Runtime runtime = Runtime.getRuntime();
 			 	long usedmem = runtime.totalMemory() - runtime.freeMemory();
 			 	long availableMem = runtime.maxMemory(); 
 			 	double usedMemFraction = (double)usedmem / (double)availableMem;
 			 	CTinfo.debugPrint(cacheProfile, "DataCache stats, usedmem: "+usedmem+", size: "+size()+", availableMem: "+availableMem+", usedMemPerc: "+usedMemFraction);
-//	            return ((size() >  MAX_ENTRIES) || (usedmem > MAX_JVMSIZE));
 	            return ((size() >  MAX_ENTRIES) || (usedMemFraction > MAX_MEMUSE));
 	         }
 	};		
 
 	// ZipFileCache caches open zip files; these take significant overhead to open/close on each use
 	static LinkedHashMap<String, ZipFile> ZipFileCache = new LinkedHashMap<String, ZipFile>() {
-		 protected boolean removeEldestEntry(Map.Entry  eldest) {
-//			 System.err.println("ZipMapCache size: "+size());
-	            return ((size() >  MAX_FILES));
+		 protected synchronized boolean removeEldestEntry(Map.Entry<String, ZipFile>  eldest) {
+//			 	System.err.println("ZipFileCache size: "+size());
+			 	
+			 	// explicitly loop through close and release entries (return true not reliable?)
+			 	if(size() > MAX_ZIPFILES) {
+			 		for (Map.Entry<String, ZipFile> entry : ZipFileCache.entrySet()) {
+			 			try{ entry.getValue().close(); } catch(Exception e){};  	// explicit close
+//			 			System.err.println("ZipFileCache remove key: "+entry.getKey());
+			 			ZipFileCache.remove(entry.getKey());						// explicit remove
+			 			if(size() <= MAX_ZIPFILES) break;
+			 		}
+//				 	System.err.println("ZipFileCache >remove size: "+ZipFileCache.size());
+			 	}
+			 	return false;
+//	            return ((size() >  MAX_FILES));
 	         }
 	};		
 	
 	// ZipFileCache getter
-	static ZipFile cachedZipFile(String myZipFile) throws Exception {
+	static synchronized ZipFile cachedZipFile(String myZipFile) throws Exception {
 		ZipFile thisZipFile = ZipFileCache.get(myZipFile);
 		if(thisZipFile == null) {
 			CTinfo.debugPrint(cacheProfile,"OPEN ZIPFILE");
+//			System.err.println("OPEN ZIPFILE: "+myZipFile);
 			thisZipFile = new ZipFile(myZipFile);
 			ZipFileCache.put(myZipFile, thisZipFile);
 		}
