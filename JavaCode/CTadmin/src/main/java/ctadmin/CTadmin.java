40ed5e8c3120c7f42c2a3c33f54b7d20a0d47681
Matt Miller <matt@cycronix.com>
2018-04-11 14:55:19 -0400

    CTadmin: add another note

--- JavaCode/CTadmin/src/main/java/ctadmin/CTadmin.java
+++ JavaCode/CTadmin/src/main/java/ctadmin/CTadmin.java
@@ -25,7 +25,8 @@ limitations under the License.
 /* Known issues (4/11/18):
  * "Repack" is limited to continuous time-sequence values, use with caution.  See CTpack.java for more details.
  * Channels popup is not (yet) functional
- * Some quirks on Windows renaming/deleting files (stray open files)
+ * Some issues on Windows renaming/deleting files (stray open files)
+ * Some quirks regarding UI refreshing properly.
  */
 
 package ctadmin;
